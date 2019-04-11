10658674536f4df6f7c92bb04d6e2d1709d4c1a3
Matthew Abebe <matthewabebe.inbox@gmail.com>
2019-04-11 09:25:06 -0500

    different solutions to practice

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,27 @@ const myName = "Matthew"
 document.querySelector(".article__header").textContent= `Welcome to the ${myName} blog!`;
 console.log(document.querySelector(".article__header"))
 
-document.querySelector(".article__header").classList.add("important")
+// Using for loop. //
+
+const articleHeaderEl = document.querySelectorAll(".article__header")
+for (let i = 0; i < articleHeaderEl.length; i++) {
+    articleHeaderEl[i].classList.add("important");
+}
+console.log(articleHeaderEl[0]);
+
+// const elements = document.querySelectorAll(".article__header")
+// console.log("elements", elements);
+
+// Using bracket notation. //
+
+// elements[0].classList.add("important");
+// elements[1].classList.add("important");
+
+// Using forEach loop. "header" equivalent to "articleHeaderEl[i] in for loop above." //
+
+// elements.forEach(header => {
+//     header.classList.add("important");
+// })
 
 const dashEl = document.querySelector(".dashed")
 dashEl.classList.remove("dashed");
